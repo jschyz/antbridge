7220eb057771119e8ccedb8d4ff4fa361ab0b8f8
huihui <skyalpha@126.com>
2017-02-23 17:26:48 +0800

    Repair the cover bugs:bable-plugin-istanbul

--- build/karma.cover.config.js
+++ build/karma.cover.config.js
@@ -3,20 +3,23 @@ var base = require('./karma.base.config')
 module.exports = function(config) {
   var options = Object.assign(base, {
     reporters: ['mocha', 'coverage'],
-    coverageReporter:{
-      dir: '../coverage',
+    coverageReporter: {
       reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary', subdir: '.' }
+        { type: 'lcov', dir: '../coverage', subdir: '.' },
+        { type: 'text-summary', dir: '../coverage', subdir: '.' }
       ]
     },
     browsers: ['PhantomJS'],
-    singleRun: false,
+    singleRun: true,
     plugins: base.plugins.concat([
       'karma-coverage',
       'karma-phantomjs-launcher'
     ])
   })
+
+  options.webpack.module.rules[0].options = {
+    plugins: ['istanbul']
+  }
 
   config.set(options)
 }
